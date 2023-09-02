a55463d8d8b00b6b18c0353822e77ffb91f8b3ec
Joel Anang <133300552+Jo-erl@users.noreply.github.com>
2023-09-02 23:28:36 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,621 @@
+const flags = [
+    {
+        flagPath: 'flags/Afghanistan.jpg',
+        capital: 'Kabul',
+        continent: 'Asia',
+        correctAnswer: 'Afghanistan',
+        wrongAnswers: ['Serbia', 'Albania'],
+    },
+
+    {
+        flagPath: 'flags/Albania.jpg',
+        capital: 'Tirana',
+        continent: 'Europe',
+        correctAnswer: 'Albania',
+        wrongAnswers: ['Croatia', 'Italy'],
+    },
+
+    {
+        flagPath: 'flags/Algeria.jpg',
+        capital: 'Algiers',
+        continent: 'Africa',
+        correctAnswer: 'Algeria',
+        wrongAnswers: ['Morocco', 'Tunisia'],
+    },
+
+    {
+        flagPath: 'flags/Andorra.jpg',
+        capital: 'Andorra la Vella',
+        continent: 'Europe',
+        correctAnswer: 'Andorra',
+        wrongAnswers: ['Zambia', 'Romania'],
+    },
+
+    {
+        flagPath: 'flags/Angola.jpg',
+        capital: 'Luanda',
+        continent: 'Africa',
+        correctAnswer: 'Angola',
+        wrongAnswers: ['Namibia', 'Zambia'],
+    },
+
+    {
+        flagPath: 'flags/Antigua_and_Barbuda.jpg',
+        capital: '	St. Johns',
+        continent: 'North America',
+        correctAnswer: 'Antigua and Barbuda',
+        wrongAnswers: ['Barbados', 'Saint Kitts and Nevis'],
+    },
+
+{
+        flagPath: 'flags/Argentina.jpg',
+        capital: '	Buenos Aires',
+        continent: 'South America',
+        correctAnswer: 'Argentina',
+        wrongAnswers: ['Brazil', 'Chile'],
+    },
+
+{
+        flagPath: 'flags/Armenia.jpg',
+        capital: 'Yerevan',
+        continent: 'Asia',
+        correctAnswer: 'Armenia',
+        wrongAnswers:  ['Georgia', 'Azerbaijan'],
+    },
+
+
+{
+        flagPath: 'flags/Australia.jpg',
+        capital: 'Canberra',
+        continent: 'Oceania',
+        correctAnswer: 'Australia',
+        wrongAnswers: ['New Zealand', 'Indonesia'],
+    },
+/////////////////////////////////////////////////////////////
+
+{
+        flagPath: 'flags/Austria.jpg',
+        capital: 'Vienna',
+        continent: 'Europe',
+        correctAnswer: 'Austria',
+        wrongAnswers: ['Germany', 'Czech Republic'],
+    },
+
+{
+        flagPath: 'flags/Azerbaijan.jpg',
+        capital: 'Canberra',
+        continent: 'Oceania',
+        correctAnswer: 'Azerbaijan',
+        wrongAnswers: ['Georgia', 'Armenia'],
+    },
+
+{
+        flagPath: 'flags/Bahamas.jpg',
+        capital: 'Nassau',
+        continent: 'North America',
+        correctAnswer: 'Bahamas',
+        wrongAnswers: ['Cuba', 'Haiti'],
+    },
+
+{
+        flagPath: 'flags/Bahrain.jpg',
+        capital: 'Manama',
+        continent: 'Asia',
+        correctAnswer: 'Bahrain',
+        wrongAnswers: ['Qatar', 'Saudi Arabia'],
+    },
+
+{
+        flagPath: 'flags/Bangladesh.jpg',
+        capital: 'Dhaka',
+        continent: 'Asia',
+        correctAnswer: 'Bangladesh',
+        wrongAnswers:  ['India', 'Myanmar'],
+    },
+
+{
+        flagPath: 'flags/Barbados.jpg',
+        capital: 'Bridgetown',
+        continent: 'North America',
+        correctAnswer: 'Barbados',
+        wrongAnswers: ['Saint Lucia', 'Grenada'],
+    },
+
+{
+        flagPath: 'flags/Belarus.jpg',
+        capital: 'Minsk',
+        continent: 'Europe',
+        correctAnswer: 'Belarus',
+        wrongAnswers:  ['Russia', 'Ukraine'],
+    },
+
+{
+        flagPath: 'flags/Belgium.jpg',
+        capital: 'Brussels',
+        continent: 'Europe',
+        correctAnswer: 'Belgium',
+        wrongAnswers:  ['Netherlands', 'France'],
+    },
+
+{
+        flagPath: 'flags/Benin.jpg',
+        capital: 'Porto Novo',
+        continent: 'Africa',
+        correctAnswer: 'Benin',
+        wrongAnswers:  ['Nigeria', 'Togo'],
+    },
+
+{
+        flagPath: 'flags/Bhutan.jpg',
+        capital: 'Thimphu',
+        continent: 'Asia',
+        correctAnswer: 'Bhutan',
+        wrongAnswers:  ['India', 'China'],
+    },
+/////////////////////////////////////////////////////////////
+
+{
+    flagPath: 'flags/Belize.jpg',
+    capital: 'Belmopan',
+    continent: 'North America',
+    correctAnswer: 'Belize',
+    wrongAnswers:   ['Guatemala', 'Mexico'],
+},
+
+{
+    flagPath: 'flags/Bolivia.jpg',
+    capital: 'Sucre',
+    continent: 'South America',
+    correctAnswer: 'Bolivia',
+    wrongAnswers:  ['Peru', 'Chile'],
+},
+
+{
+    flagPath: 'flags/Bosnia_and_Herzegovina.jpg',
+    capital: 'Sarajevo',
+    continent: 'Europe',
+    correctAnswer: 'Bosnia and Herzegovina',
+    wrongAnswers:  ['Serbia', 'Croatia'],
+},
+
+{
+    flagPath: 'flags/Botswana.jpg',
+    capital: 'Gaborone',
+    continent: 'Africa',
+    correctAnswer: 'Botswana',
+    wrongAnswers:  ['Namibia', 'Zimbabwe'],
+},
+
+{
+    flagPath: 'flags/Brazil.jpg',
+    capital: 'Brasilia',
+    continent: 'South America',
+    correctAnswer: 'Brazil',
+    wrongAnswers:  ['Argentina', 'Uruguay'],
+},
+
+{
+    flagPath: 'flags/Brunei.jpg',
+    capital: 'Bandar Seri Begawan',
+    continent: 'Asia',
+    correctAnswer: 'Brunei',
+    wrongAnswers:   ['Malaysia', 'Indonesia'],
+},
+
+{
+    flagPath: 'flags/Bulgaria.jpg',
+    capital: 'Sofia',
+    continent: 'Europe',
+    correctAnswer: 'Bulgaria',
+    wrongAnswers:  ['Greece', 'Romania'],
+},
+
+{
+    flagPath: 'flags/Burkina_Faso.jpg',
+    capital: 'Ouagadougou',
+    continent: 'Africa',
+    correctAnswer: 'Burkina Faso',
+    wrongAnswers:   ['Niger', 'Mali'],
+},
+
+{
+    flagPath: 'flags/Burundi.jpg',
+    capital: 'Bujumbura',
+    continent: 'Africa',
+    correctAnswer: 'Burundi',
+    wrongAnswers:  ['Rwanda', 'Tanzania'],
+},
+
+{
+    flagPath: 'flags/Cambodia.jpg',
+    capital: 'Phnom Penh',
+    continent: 'Asia',
+    correctAnswer: 'Cambodia',
+    wrongAnswers:  ['Thailand', 'Vietnam'],
+},
+/////////////////////////////////////////////////
+
+{
+    flagPath: 'flags/Cameroon.jpg',
+    capital: 'Yaoundé',
+    continent: 'Africa',
+    correctAnswer: 'Cameroon',
+    wrongAnswers:  ['Thailand', 'Vietnam'],
+},
+
+{
+    flagPath: 'flags/Canada.jpg',
+    capital: 'Ottawa',
+    continent: 'North America',
+    correctAnswer: 'Canada',
+    wrongAnswers:  ['Thailand', 'Vietnam'],
+},
+
+{
+    flagPath: 'flags/Cape_Verde.jpg',
+    capital: 'Praia',
+    continent: 'Africa',
+    correctAnswer: 'Cape_Verde',
+    wrongAnswers:   ['Senegal', 'Mauritania'],
+},
+
+{
+    flagPath: 'flags/Central_African_ Republic.jpg',
+    capital: 'Bangui',
+    continent: 'Africa',
+    correctAnswer: 'Central African Republic',
+    wrongAnswers:  ['Chad', 'Sudan'],
+},
+
+{
+    flagPath: 'flags/Chad.jpg',
+    capital: 'N\'Djamena',
+    continent: 'Africa',
+    correctAnswer: 'Chad',
+    wrongAnswers:   ['Nigeria', 'Sudan'],
+},
+
+{
+    flagPath: 'flags/Chile.jpg',
+    capital: 'Santiago',
+    continent: 'South America',
+    correctAnswer: 'Chile',
+    wrongAnswers:  ['Argentina', 'Peru'],
+},
+
+{
+    flagPath: 'flags/China.jpg',
+    capital: 'Beijing',
+    continent: 'Asia',
+    correctAnswer: 'China',
+    wrongAnswers:   ['Russia', 'India'],
+},
+
+{
+    flagPath: 'flags/Colombia.jpg',
+    capital: 'Bogota',
+    continent: 'South America',
+    correctAnswer: 'Colombia',
+    wrongAnswers:  ['Venezuela', 'Peru'],
+},
+
+{
+    flagPath: 'flags/Comoros.jpg',
+    capital: 'Moroni',
+    continent: 'Africa',
+    correctAnswer: 'Comoros',
+    wrongAnswers:  ['Madagascar', 'Mozambique'],
+},
+
+{
+    flagPath: 'flags/Costa_Rica.jpg',
+    capital: 'San José',
+    continent: 'North America',
+    correctAnswer: 'Costa Rica',
+    wrongAnswers:  ['Nicaragua', 'Panama'],
+},
+///////////////////////////////////////////////
+{
+    flagPath: 'flags/Croatia.jpg',
+    capital: 'Zagreb',
+    continent: 'Europe',
+    correctAnswer: 'Croatia',
+    wrongAnswers:   ['Slovenia', 'Italy'],
+},
+
+{
+    flagPath: 'flags/Cuba.jpg',
+    capital: 'Havana',
+    continent: 'North America',
+    correctAnswer: 'Cuba',
+    wrongAnswers:  ['Jamaica', 'Haiti'],
+},
+
+{
+    flagPath: 'flags/Cyprus.jpg',
+    capital: 'Nicosia',
+    continent: 'Europe',
+    correctAnswer: 'Cyprus',
+    wrongAnswers:   ['Greece', 'Turkey'],
+},
+
+{
+    flagPath: 'flags/Czech Republic.jpg',
+    capital: 'Prague',
+    continent: 'Prague',
+    correctAnswer: 'Europe',
+    wrongAnswers:  ['Germany', 'Austria'],
+},
+
+{
+    flagPath: 'flags/Democratic_Republic_of_Congo.jpg',
+    capital: 'Kinshasa',
+    continent: 'Africa',
+    correctAnswer: 'Democratic Republic of Congo',
+    wrongAnswers: ['Republic of the Congo', 'Uganda'],
+},
+
+{
+    flagPath: 'flags/Denmark.jpg',
+    capital: 'Copenagen',
+    continent: 'Europe',
+    correctAnswer: 'Denmark',
+    wrongAnswers:   ['Germany', 'Sweden'],
+},
+
+{
+    flagPath: 'flags/Djibouti.jpg',
+    capital: 'Djibouti',
+    continent: 'Africa',
+    correctAnswer: 'Djibouti',
+    wrongAnswers:   ['Somalia', 'Eritrea'],
+},
+
+{
+    flagPath: 'flags/Dominica.jpg',
+    capital: 'Roseau',
+    continent: 'North America',
+    correctAnswer: 'Dominica',
+    wrongAnswers:   ['Saint Lucia', 'Grenada'],
+},
+
+{
+    flagPath: 'flags/Dominican_Republic.jpg',
+    capital: 'Santo Domingo',
+    continent: 'North America',
+    correctAnswer: 'Dominican Republic',
+    wrongAnswers:  ['Haiti', 'Cuba'],
+},
+
+{
+    flagPath: 'flags/East_Timor.jpg',
+    capital: 'Dili',
+    continent: 'Oceania',
+    correctAnswer: 'East Timor',
+    wrongAnswers:   ['Indonesia', 'Australia'],
+},
+
+{
+    flagPath: 'flags/Ecuador.jpg',
+    capital: 'Quito',
+    continent: 'South America',
+    correctAnswer: 'Ecuador',
+    wrongAnswers:   ['Peru', 'Colombia'],
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Add data for other flags
+];
+
+let currentFlagIndex = 0;
+let score = 0;
+let shuffledFlags = [...flags]; // Create a shuffled copy of the flags array
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array; // Add this line to return the shuffled array
+}
+
+function shuffleFlags() {
+    shuffledFlags = [...flags]; // Reset shuffledFlags to the original array
+    shuffleArray(shuffledFlags); // Shuffle the flags array
+}
+
+
+function loadFlag() {
+    if (currentFlagIndex < shuffledFlags.length) {
+        const flag = shuffledFlags[currentFlagIndex];
+        document.getElementById('flag').src = flag.flagPath;
+        document.getElementById('hint').innerHTML = `<strong>Capital:</strong> ${flag.capital}, <strong>Continent:</strong> ${flag.continent}`;
+        const options = shuffleArray([flag.correctAnswer, ...flag.wrongAnswers]);
+        options.forEach((option, index) => {
+            document.getElementsByClassName('option')[index].textContent = option;
+        });
+    } else {
+        endGame();
+    }
+}
+
+function checkAnswer(button) {
+    const selectedAnswer = button.textContent;
+    const flag = shuffledFlags[currentFlagIndex];
+    const hintElement = document.getElementById('hint');
+
+    if (selectedAnswer === flag.correctAnswer) {
+        score++;
+        document.getElementById('score').textContent = `Score: ${score}`;
+        hintElement.textContent = 'Correct!';
+        hintElement.style.color = 'green'; // Set text color to green for correct answer
+    } else {
+        hintElement.textContent = `Answer: ${flag.correctAnswer}`;
+        hintElement.style.color = 'red'; // Set text color to red for wrong answer
+    }
+
+    currentFlagIndex++;
+    setTimeout(() => {
+        hintElement.textContent = ''; // Clear the message
+        hintElement.style.color = ''; // Reset text color
+        loadFlag();
+    }, 1600); 
+}
+
+function showMessage(message, color) {
+    const messageElement = document.getElementById('hint');
+    messageElement.textContent = message;
+    messageElement.style.color = color;
+}
+
+function startGame() {
+    currentFlagIndex = 0;
+    score = 0;
+    document.getElementById('score').textContent = 'Score: 0';
+    document.getElementById('hint').textContent = '';
+    document.getElementById('flag').style.display = 'block';
+    document.getElementById('options').style.display = 'block';
+    document.getElementById('play-again').style.display = 'none';
+
+    shuffleFlags(); // Shuffle the flags array
+    loadFlag(); // Load the first flag
+}
+
+window.onload = startGame;
+
+
+function endGame() {
+    const totalFlags = flags.length;
+    const scoreText = `Score: ${score} /  ${totalFlags}`;
+    document.getElementById('hint').textContent = 'Game Over!';
+    document.getElementById('score').textContent = scoreText;
+    document.getElementById('flag').style.display = 'none'; // Hide the flag
+    document.getElementById('options').style.display = 'none';
+    document.getElementById('play-again').style.display = 'block'; // Show the Play Again button
+}
+
+function playAgain() {
+    currentFlagIndex = 0;
+    score = 0;
+    document.getElementById('score').textContent = 'Score: 0';
+    document.getElementById('hint').textContent = '';
+    document.getElementById('flag').style.display = 'block';
+    document.getElementById('options').style.display = 'block';
+    document.getElementById('play-again').style.display = 'none';
+
+    shuffleFlags(); // Shuffle the flags array
+    loadFlag(); // Load the first flag
+}
+
+
+
+function showMessage(message) {
+    document.getElementById('hint').textContent = message;
+}
+
+document.getElementById('play-again').style.display = 'none'; // Hide the Play Again button initially
+
+loadFlag();
